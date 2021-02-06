712f8f8e2816643f6f2cb49ce44482aede034904
Choi Jiwon <winning1993@naver.com>
2021-02-07 06:18:22 +0900

    [ADD] 비디오 흑백 효과 테스트 & README.md 추가

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import cv2
 import numpy
 
-testImg = cv2.imread("Resources/react-logo.png", 0)
+# 이미지 흑백 처리
+# testImg = cv2.imread("Resources/react-logo.png", 0)
+#
+# resizedImg = cv2.resize(testImg, (800,600))
+#
+#
+# cv2.imshow("testImg", resizedImg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-resizedImg = cv2.resize(testImg, (800,600))
+# 동영상 흑백 처리
 
+testCapture = cv2.VideoCapture("Resources/test_video.mp4")
 
-cv2.imshow("testImg", resizedImg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+while True:
+    ret, frame = testCapture.read()
+
+    # ret은 frame을 받는 지 안 받는지 boolean 값으로 알려주는 것으로 보임.
+    if not ret:
+        print("Can't receive frame")
+        break
+    print(ret)
+    grayVideo = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', grayVideo)
+    if cv2.waitKey(1) == ord('q'):
+        break
